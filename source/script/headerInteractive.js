05908f2c72cd737cb352f9c634a69ad5ee04afe2
jonnpk <soondae33@gmail.com>
2024-11-20 00:48:22 +0900

    241120

--- source/script/headerInteractive.js
+++ source/script/headerInteractive.js
@@ -6,13 +6,7 @@ const ratio = window.innerHeight;
 window.addEventListener('scroll', () => {
     if (window.scrollY > ratio) {
         header.classList.add('scrolled');
-        // Logo paths and hamburger paths change to black
-        logoPaths.forEach(path => (path.style.fill = '#151515'));
-        hamburgerPaths.forEach(path => (path.style.fill = '#151515'));
     } else {
         header.classList.remove('scrolled');
-        // Revert logo paths and hamburger paths to original color
-        logoPaths.forEach(path => (path.style.fill = 'white'));
-        hamburgerPaths.forEach(path => (path.style.fill = 'white'));
     }
 });
