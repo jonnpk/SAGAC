568e30922abb64b8f0e6308173e3d9d2dee51c8d
jonn <soondae33@gmail.com>
2024-11-21 15:04:25 +0900

    241121

--- source/script/fadeInScroll.js
+++ source/script/fadeInScroll.js
@@ -1,15 +1,17 @@
-// 페이드인 스크롤
 const content = document.getElementsByClassName("fade-interactive");
+
 window.addEventListener("scroll", () => {
     const winH = window.innerHeight;
 
     for (let i = 0; i < content.length; i++) {
         const contentTop = content[i].getBoundingClientRect().top;
+        const contentBottom = content[i].getBoundingClientRect().bottom;
 
-        if (contentTop - winH < 0) {
+        if (contentTop - winH < 0 && contentBottom > 0) {
+            // 요소가 뷰포트 안에 있을 때
             content[i].classList.add("in");
-        }
-        if (contentTop - winH > 0) {
+        } else {
+            // 요소가 뷰포트를 완전히 벗어났을 때
             content[i].classList.remove("in");
         }
     }
